514dd5adf0d8d6f01a2a5d4af715796437524854
Olivia Letendart <letendartolivia@gmail.com>
2020-01-09 17:25:49 +0100

    create new const key in the file script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("keydown", function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function
+  audio.currentTime = 0;
+  audio.play();
+  console.log(key);
+});
