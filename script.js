858ce18a260fb936674118459105b19b8b2e7cf1
Olivia Letendart <letendartolivia@gmail.com>
2020-01-09 17:48:10 +0100

    in the function removeTransition, if + remove class playing

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ window.addEventListener("keydown", function(e) {
 });
 
 function removeTransition(e) {
-  console.log(e);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
